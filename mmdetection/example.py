8c552ca651b9ee9f64281862dabfc970d414b38e
lyh <liuyunhaozz@gmail.com>
2021-07-29 13:11:59 +0800

    开始, 加入一个demo文件夹

--- mmdetection/example.py
+++ mmdetection/example.py
@@ -1,11 +1,14 @@
 from mmdet.apis import init_detector, inference_detector
+import mmcv
 
 config_file = 'configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
 # 从 model zoo 下载 checkpoint 并放在 `checkpoints/` 文件下
 # 网址为: http://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_1x_coco/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth
 checkpoint_file = 'checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
-device = 'cuda:0'
-# 初始化检测器
-model = init_detector(config_file, checkpoint_file, device=device)
-# 推理演示图像
-inference_detector(model, 'demo/demo.jpg')
+model = init_detector(config_file, checkpoint_file, device='cuda:0')
+
+# 测试单张图片并展示结果
+img = 'demo/demo.jpg'  # 或者 img = mmcv.imread(img)，这样图片仅会被读一次
+result = inference_detector(model, img)
+# 在一个新的窗口中将结果可视化
+model.show_result(img, result,out_file = 'result.jpg')
